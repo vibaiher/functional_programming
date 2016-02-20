1e28122e38d3501e41ee68bf7c178080d558acd7
Vicente Baixauli <vibaiher@gmail.com>
2016-02-20 23:43:15 +0000

    Watchlist. Initial commit

---
+++ elixir/watchlist/web/router.ex
@@ -0,0 +1,11 @@
+defmodule Watchlist.Router do
+  use Watchlist.Web, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", Watchlist do
+    pipe_through :api
+  end
+end
